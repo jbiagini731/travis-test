40b8b80a65ef670fad32a5f0c2a3f353f1d3de68
jbiagini731 <41298771+jbiagini731@users.noreply.github.com>
2020-10-14 13:49:26 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -8,10 +8,11 @@ s3 = boto3.resource('s3')
 test_bucket = 'arcadia-desktop-automation-resources-repo'
 test_file   = 'test_file.txt'
 tries = 0
-
-while tries < 5:
+results_file = False
+while tries < 5 and not results_file:
   try:
     s3.Object(test_bucket, test_file).load()
+    results_file = True
   except Exception as e:
     print('Exception:' + str(e))
     print('Try number: ' + str(tries))
