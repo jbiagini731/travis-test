4e95cc62b386936daf238fdfb2c7637f70cf6461
jbiagini731 <41298771+jbiagini731@users.noreply.github.com>
2020-10-14 13:30:07 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,7 +2,6 @@
 
 import boto3
 import botocore
-#import sys
 import time
 
 s3 = boto3.resource('s3')
@@ -13,20 +12,11 @@ tries = 0
 while tries < 5:
   try:
     s3.Object(test_bucket, test_file).load()
-  except botocore.exceptions.ClientError as e:
+  except Exception as e:
+    print('Exception:' + str(e))
     print('Try number: ' + str(tries))
-    if e.response['Error']['Code'] == "404":
-      print('Object does not exist, yet...')
-      tries+=1
-      time.sleep(10)
+    tries+=1
+    time.sleep(10)
     else:
       print('An unforseen issue has occurred')
       exit(1)
-    raise
-
-#print('PID: ' + str(sys.argv[1]))
-
-#if int(sys.argv[1]) > 0:
-#  exit(0)
-#else:
-#  exit(1)
