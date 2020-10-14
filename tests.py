ca6452e698dc75de85ad8e0a2ea0272ab4c119f4
jbiagini731 <41298771+jbiagini731@users.noreply.github.com>
2020-10-14 13:12:39 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python
 
-import sys
+import boto3
+import botocore
+#import sys
+import time
 
-print('PID: ' + str(sys.argv[1]))
+s3 = boto3.resource('s3')
+test_bucket = 'arcadia-desktop-automation-resources-repo'
+test_file   = 'test_file.txt'
+tries = 0
 
-if int(sys.argv[1]) > 0:
-  exit(0)
-else:
-  exit(1)
+while tries < 5:
+  try:
+    s3.Object(test_bucket, test_file).load()
+  except botocore.exceptions.ClientError as e:
+    print('Try number: ' + tries)
+    if e.response['Error']['Code'] == "404":
+      print('Object does not exist, yet...')
+      tries+=1
+    else:
+      print('An unforseen issue has occurred')
+      exit(1)
+    raise
+
+#print('PID: ' + str(sys.argv[1]))
+
+#if int(sys.argv[1]) > 0:
+#  exit(0)
+#else:
+#  exit(1)
